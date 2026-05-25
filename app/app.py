2b881a038c063d8a6b9676127f0e2d4a960cd6e5
Sohan Gurav <73344291+sohang05@users.noreply.github.com>
2026-05-25 21:18:36 +0530

    Added Prometheus metrics

--- app/app.py
+++ app/app.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask
+from prometheus_client import Counter, generate_latest
+from flask import Response
 
 app = Flask(__name__)
 
+REQUEST_COUNT = Counter(
+    'app_requests_total',
+    'Total app requests'
+)
+
 @app.route("/")
 def home():
-    return render_template("index.html", title="Flask Dashboard")
+    REQUEST_COUNT.inc()
+    return "Flask app running"
+
+@app.route("/metrics")
+def metrics():
+    return Response(
+        generate_latest(),
+        mimetype="text/plain"
+    )
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",port=5000)
+    app.run(host="0.0.0.0", port=5000)
