3dab1dda3e72b603e2878d9187353344526f5f0d
Sohan Gurav <73344291+sohang05@users.noreply.github.com>
2026-05-27 17:12:05 +0530

    Automate monitoring deployment

--- terraform/sg/main.tf
+++ terraform/sg/main.tf
@@ -18,6 +18,14 @@ resource "aws_security_group" "allow_ssh_http" {
   }
 
   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
